261f1ef294c07219d0b9906164a4bd855c7333a8
yop yop yop <hoholee12@naver.com>
2016-07-09 01:43:46 +0900

    fsx

--- fsx.bat
+++ fsx.bat
@@ -1,9 +1,9 @@
-@echo off
+
 dc64 -width=1280 -height=800
-start /wait steam -applaunch 314160
+start "" "fsx.exe"
 set iamset=0
 :loop
-sleep 5
+sleep 1
 tasklist /FI "IMAGENAME eq fsx.exe" 2>NUL | find /I /N "fsx.exe">NUL
 if %ERRORLEVEL% == 1 (
 	if %iamset% == 1 (

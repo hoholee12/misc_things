e95105c46756e83e7ded77ad0ef5aa2d8ab8c1b5
yop yop yop <hoholee12@naver.com>
2016-08-06 02:17:52 +0900

    gta5

--- gta5.bat
+++ gta5.bat
@@ -1,10 +1,10 @@
 @echo off
-dc64 -width=1280 -height=800
-start "" "fsx.exe"
+dc64 -width=1280 -height=720
+steam -applaunch 271590
 set iamset=0
 :loop
 sleep 1
-tasklist /FI "IMAGENAME eq fsx.exe" 2>NUL | find /I /N "fsx.exe">NUL
+tasklist /FI "IMAGENAME eq GTA5.exe" 2>NUL | find /I /N "GTA5.exe">NUL
 if %ERRORLEVEL% == 1 (
 	if %iamset% == 1 (
 		dc64 -width=max -height=max

15b8e85c30c164455ce4058b34368e533e4150ac
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2016-08-23 20:30:43 +0900

    wait till service starts

--- xtu.bat
+++ xtu.bat
@@ -1,5 +1,11 @@
-start /wait xtucli -t -id 59 -v 9.5
-start /wait xtucli -t -id 59 -v 9.0
-start /wait xtucli -t -id 84 -v 33.000
-start /wait xtucli -t -id 84 -v 32.875
-
+for /F "tokens=3 delims=: " %%H in ('sc query "XTU3SERVICE" ^| findstr "        상태"') do (
+	if /I "%%H" NEQ "RUNNING" (
+		exit /b
+	)
+	timeout 1
+)
+for /L %%n in (1,0,10) do (
+	start /wait xtucli -t -id 59 -v 9.0
+	start /wait xtucli -t -id 59 -v 8.5
+	timeout 300
+)
